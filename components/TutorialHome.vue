bcbf8eeff93765a01610c8fef9b75879a61aa7cb
knight.gao <knight.gao@tineco.com>
2023-01-29 17:12:25 +0800

    init: 初始化

---
+++ components/TutorialHome.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <nav>
+      <a v-for="item in linkList" :key="item.id" :href="item.href">
+        {{ item.name }}
+      </a>
+    </nav>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+// const title = ref('秋浦の能量站')
+
+const linkList = ref([
+  {
+    id: '1',
+    name: 'home',
+    href: 'https://devgaoy.cn'
+  }
+]);
+</script>
